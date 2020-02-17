04cd85d17d07fec34330829b7f65da1f0b2d6b10
Vlad Darienco <vlad.lopatinsky@gmail.com>
2020-02-17 00:35:59 -0800

    exercise 1.11

---
+++ ch1/exercise1.11.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int num1, num2;
+
+	cin >> num1 >> num2;
+
+	for (num1; num1<=num2; num1++)
+	{
+		cout << num1 << endl;
+	}
+
+	system("Pause");
+}
